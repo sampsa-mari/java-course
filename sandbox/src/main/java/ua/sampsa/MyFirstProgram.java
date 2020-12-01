d32ed2228cbffa50cf20ad1656a5d0936c7d993c
Tkachuk Maryna <mkotenko@gmail.com>
2020-12-01 20:27:12 +0100

    Added several functions

--- sandbox/src/main/java/ua/sampsa/MyFirstProgram.java
+++ sandbox/src/main/java/ua/sampsa/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ua.sampsa;
 public class MyFirstProgram {
 
     public static void main(String args[]) {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("user");
+        hello("Aleksei");
+
+        double length = 5;
+        System.out.println("The area of square with side length " + length + " = " + area(length));
+
+        double a = 4;
+        double b = 5;
+        System.out.println("The area of a rectangle with a sides length " + a + " and " + b + " = " + area(a, b));
+
     }
 
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody +"!");
+    }
+
+    public static double area(double l){
+       return l * l;
+    }
+
+    public static double area(double x, double y){
+        return x * y;
+    }
 }
