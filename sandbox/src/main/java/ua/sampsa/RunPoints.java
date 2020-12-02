3cab373493773c3345b37c1767e49845040d1a4a
Tkachuk Maryna <mkotenko@gmail.com>
2020-12-02 11:46:08 +0100

    Method in Point Class is created

--- sandbox/src/main/java/ua/sampsa/RunPoints.java
+++ sandbox/src/main/java/ua/sampsa/RunPoints.java
@@ -6,19 +6,10 @@ package ua.sampsa;
 public class RunPoints {
   public static void main(String args[]) {
 
-    Point p1 = new Point();
-    Point p2 = new Point();
+    Point p1 = new Point(1,3);
+    Point p2 = new Point(2,4);
 
-    p1.x = 1;
-    p2.x = 2;
-    p1.y = 3;
-    p2.y = 4;
-
-    System.out.println("The distance between 2 points - A and B with the coordinates: PointA (x1 - " + p1.x + ", y1 - " + p1.y + "), and PointB (x2 - " + p2.x + ", y2 - " + p2.y + ") = " + distance(p1, p2));
+    System.out.println("The distance between 2 points - A and B with the coordinates: PointA (x1 - " + p1.x + ", y1 - " + p1.y + "), and PointB (x2 - " + p2.x + ", y2 - " + p2.y + ") = " + Point.distance(p1, p2));
   }
 
-    public static double distance(Point p1, Point p2){
-      return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-    }
-
-  }
+}
