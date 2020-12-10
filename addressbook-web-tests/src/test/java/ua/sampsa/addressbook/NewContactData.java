453a32839c0d503e06ebe6f7141ec18b103ecb6c
Tkachuk Maryna <mkotenko@gmail.com>
2020-12-10 15:46:48 +0100

    New Object for all parameters for New Contact creation

---
+++ addressbook-web-tests/src/test/java/ua/sampsa/addressbook/NewContactData.java
@@ -0,0 +1,79 @@
+package ua.sampsa.addressbook;
+
+public class NewContactData {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String nickName;
+  private final String companyName;
+  private final String address;
+  private final String mobilePhone;
+  private final String email;
+  private final String dayOfBirth;
+  private final String monthOfBirth;
+  private final String yearOfBirth;
+  private final String group;
+
+  public NewContactData(String firstName, String middleName, String lastName, String nickName, String companyName, String address, String mobilePhone, String email, String dayOfBirth, String monthOfBirth, String yearOfBirth, String group) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickName = nickName;
+    this.companyName = companyName;
+    this.address = address;
+    this.mobilePhone = mobilePhone;
+    this.email = email;
+    this.dayOfBirth = dayOfBirth;
+    this.monthOfBirth = monthOfBirth;
+    this.yearOfBirth = yearOfBirth;
+    this.group = group;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickName() {
+    return nickName;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobilePhone() {
+    return mobilePhone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getDayOfBirth() {
+    return dayOfBirth;
+  }
+
+  public String getMonthOfBirth() {
+    return monthOfBirth;
+  }
+
+  public String getYearOfBirth() {
+    return yearOfBirth;
+  }
+
+  public String getGroup() {
+    return group;
+  }
+}
