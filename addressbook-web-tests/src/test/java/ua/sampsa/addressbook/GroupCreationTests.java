bd14345dd5b3b926de6f81ebb731ef392e1a1347
Tkachuk Maryna <mkotenko@gmail.com>
2020-12-10 13:03:44 +0100

    Methods created

--- addressbook-web-tests/src/test/java/ua/sampsa/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/sampsa/addressbook/GroupCreationTests.java
@@ -20,6 +20,10 @@ public class GroupCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php");
+    login();
+  }
+
+  private void login() {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
@@ -30,9 +34,26 @@ public class GroupCreationTests {
 
   @Test
   public void testGroupCreation() throws Exception {
+    goToGroupPage();
+    initGroupCreation();
+    fillGroupForm();
+    submitGroupCreation();
+    returnToGroupPage();
+  }
 
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm() {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
     wd.findElement(By.name("group_name")).sendKeys("newGroup");
@@ -40,13 +61,21 @@ public class GroupCreationTests {
     wd.findElement(By.name("group_header")).sendKeys("new1");
     wd.findElement(By.name("group_footer")).clear();
     wd.findElement(By.name("group_footer")).sendKeys("new2");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("group page")).click();
-    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void goToGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod
-  public void tearDown() { wd.quit(); }
+  public void tearDown() {
+    logout();
+    wd.quit();
+  }
 
   private boolean isAlertPresent() {
     try {
