685d2c3336a3acf385fda48ad4d0a8c5869b8a2d
Tkachuk Maryna <mkotenko@gmail.com>
2020-12-10 12:04:08 +0100

    Move login part to BeforeMethod

--- addressbook-web-tests/src/test/java/ua/sampsa/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/sampsa/addressbook/GroupCreationTests.java
@@ -19,10 +19,6 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/index.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -30,6 +26,11 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
@@ -42,23 +43,10 @@ public class GroupCreationTests {
     wd.findElement(By.name("submit")).click();
     wd.findElement(By.linkText("group page")).click();
     wd.findElement(By.linkText("Logout")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+  @AfterMethod
+  public void tearDown() { wd.quit(); }
 
   private boolean isAlertPresent() {
     try {
