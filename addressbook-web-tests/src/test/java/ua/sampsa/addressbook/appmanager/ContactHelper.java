efed28296361b7a96f6721487b0063f6cf28d78d
Tkachuk Maryna <mkotenko@gmail.com>
2020-12-10 23:38:50 +0100

    Click and type methods are applied to all of Helpers

--- addressbook-web-tests/src/test/java/ua/sampsa/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ua/sampsa/addressbook/appmanager/ContactHelper.java
@@ -6,50 +6,34 @@ import org.openqa.selenium.support.ui.Select;
 import ua.sampsa.addressbook.model.GroupData;
 
 
-public class ContactHelper {
-  private WebDriver wd;
+public class ContactHelper extends HelperBase{
 
   public ContactHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void submitNewContact() {
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    click(By.xpath("(//input[@name='submit'])[2]"));
   }
 
   public void fillNewContactForm(GroupData.NewContactData newContactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(newContactData.getFirstName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(newContactData.getMiddleName());
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(newContactData.getLastName());
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(newContactData.getNickName());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(newContactData.getCompanyName());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(newContactData.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(newContactData.getMobilePhone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(newContactData.getEmail());
+    type(By.name("firstname"), newContactData.getFirstName());
+    type(By.name("middlename"),newContactData.getMiddleName());
+    type(By.name("lastname"),newContactData.getLastName());
+    type(By.name("nickname"),newContactData.getNickName());
+    type(By.name("company"),newContactData.getCompanyName());
+    type(By.name("address"),newContactData.getAddress());
+    type(By.name("mobile"),newContactData.getMobilePhone());
+    type(By.name("email"),newContactData.getEmail());
+
     wd.findElement(By.name("bday")).click();
     new Select(wd.findElement(By.name("bday"))).selectByVisibleText(newContactData.getDayOfBirth());
     wd.findElement(By.xpath("//option[@value='1']")).click();
+
     wd.findElement(By.name("bmonth")).click();
     new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(newContactData.getMonthOfBirth());
     wd.findElement(By.xpath("//option[@value='January']")).click();
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(newContactData.getYearOfBirth());
 
+    type(By.name("byear"), newContactData.getYearOfBirth());
   }
 }
--- addressbook-web-tests/src/test/java/ua/sampsa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ua/sampsa/addressbook/model/GroupData.java
@@ -35,7 +35,7 @@ public class GroupData {
     private final String dayOfBirth;
     private final String monthOfBirth;
     private final String yearOfBirth;
-
+  
     public NewContactData(String firstName, String middleName, String lastName, String nickName, String companyName, String address, String mobilePhone, String email, String dayOfBirth, String monthOfBirth, String yearOfBirth) {
       this.firstName = firstName;
       this.middleName = middleName;
@@ -49,47 +49,47 @@ public class GroupData {
       this.monthOfBirth = monthOfBirth;
       this.yearOfBirth = yearOfBirth;
     }
-
+  
     public String getFirstName() {
       return firstName;
     }
-
+  
     public String getMiddleName() {
       return middleName;
     }
-
+  
     public String getLastName() {
       return lastName;
     }
-
+  
     public String getNickName() {
       return nickName;
     }
-
+  
     public String getCompanyName() {
       return companyName;
     }
-
+  
     public String getAddress() {
       return address;
     }
-
+  
     public String getMobilePhone() {
       return mobilePhone;
     }
-
+  
     public String getEmail() {
       return email;
     }
-
+  
     public String getDayOfBirth() {
       return dayOfBirth;
     }
-
+  
     public String getMonthOfBirth() {
       return monthOfBirth;
     }
-
+  
     public String getYearOfBirth() {
       return yearOfBirth;
     }
