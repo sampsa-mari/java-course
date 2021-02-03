b7902164765a8a5a67db60332f5a0ad5ae935f15
Tkachuk Maryna <mkotenko@gmail.com>
2021-02-03 22:39:41 +0100

    Added checking of number of group elements

--- addressbook-web-tests/src/test/java/ua/sampsa/addressbook/tests/GroupTests.java
+++ addressbook-web-tests/src/test/java/ua/sampsa/addressbook/tests/GroupTests.java
@@ -1,5 +1,6 @@
 package ua.sampsa.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.sampsa.addressbook.model.GroupData;
 
@@ -9,12 +10,17 @@ public class GroupTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("newGroupsName", "newGroupsHeader", "newGroupsFooter"));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before +1);
+
   }
 
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if(! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("newGroupsName", "newGroupsHeader", "newGroupsFooter"));
     }
@@ -23,16 +29,22 @@ public class GroupTests extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("GroupRename", "HeaderRename", "FooterRename"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
+
   }
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if(! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("newGroupsName", "newGroupsHeader", "newGroupsFooter"));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
   }
 }
